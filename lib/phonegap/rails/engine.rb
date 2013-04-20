3515c49f765c8507918c41975875e50b274c3519
Josep Casals <joscas@gmail.com>
2013-04-20 22:14:18 +0200

    .

--- lib/phonegap/rails/engine.rb
+++ lib/phonegap/rails/engine.rb
@@ -1,8 +1,4 @@
-#class Railtie < ::Rails::Railtie
 class Engine < ::Rails::Engine
-  # initializer 'static_assets.load_static_assets' do |app|
-  #   app.middleware.use ::ActionDispatch::Static, "#{root}/lib/assets"
-  # end
   rake_tasks do
     load "phonegap/rails/tasks/rails.rake"
   end
